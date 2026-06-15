687c97234d74d728f56b62748598c935ff021ccd
Bola Nabil <bolanabil404@gmail.com>
2026-06-15 17:49:14 +0300

    add center side links an outlet to show page links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,26 @@
 import { navLinks } from "../../utils/navLinks";
 import Logo from "../../assets/images/logo.png";
+import { Link, Outlet } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav>
-            <div className="logo">
-                <img src={Logo} alt="novacart logo"/>
-            </div>
-        </nav>
+        <>
+            <nav>
+                <div className="logo">
+                    <img src={Logo} alt="novacart logo"/>
+                </div>
+
+                <div className="center-side">
+                    {
+                        navLinks.map((link) => (
+                            <Link key={link.id} to={link.path}>{link.title}</Link>
+                        ))
+                    }
+                </div>
+            </nav>
+            
+            <Outlet />
+        </>
     );
 }
 
