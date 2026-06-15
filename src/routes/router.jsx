6c86332b170e5c5086f3af9ccc2d911873a9d639
Bola Nabil <bolanabil404@gmail.com>
2026-06-15 17:39:23 +0300

    add categories route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import Shop from "../pages/Shop/Shop";
+import Categories from "../pages/Categories/Categories";
 import Deals from "../pages/Deals/Deals";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
             {
                 "path": "/shop",
                 "element": <Shop />
+            },
+            {
+                "path": "/categories",
+                "element": <Categories />
             },
             {
                 "path": "/deals",
