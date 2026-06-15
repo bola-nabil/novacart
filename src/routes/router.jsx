86b3949607abad14aac202724d9e78244bdccabf
Bola Nabil <bolanabil404@gmail.com>
2026-06-15 16:50:08 +0300

    add home, shop, deals, contact and about pages to router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,37 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
+import Home from "../pages/Home/Home";
+import Shop from "../pages/Shop/Shop";
+import Deals from "../pages/Deals/Deals";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
     {
         "path": "/",
-        "element": <App />
+        "element": <App />,
+        "children": [
+            {
+                "index": true,
+                "element": <Home />
+            },
+            {
+                "path": "/shop",
+                "element": <Shop />
+            },
+            {
+                "path": "/deals",
+                "element": <Deals />
+            },
+            {   
+                "path": "/about",
+                "element": <About />
+            },
+            {
+                "path": "/contact",
+                "element": <Contact />
+            }
+        ]
     }
 ]);
 
