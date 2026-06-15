1d973d60c687ce12103c67a4ac26d87132a45ec2
Bola Nabil <bolanabil404@gmail.com>
2026-06-15 17:15:38 +0300

    create navLinks to include all paths of navbar

---
+++ src/utils/navLinks.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/"
+    },
+    {
+        id: "2",
+        title: "Shop",
+        path: "/shop"
+    },
+    {
+        id: "3",
+        title: "Categories",
+        path: "/categories"
+    },
+    {
+        id: "4",
+        title: "Deals",
+        path: "/deals"
+    },
+    {
+        id: "5",
+        title: "About Us",
+        path: "/about"
+    },
+    {
+        id: "5",
+        title: "Contact",
+        path: "/contact"
+    }
+];
