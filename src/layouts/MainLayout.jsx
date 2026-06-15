4ac738b204cea13c08d68e660687b288a2a32062
Bola Nabil <bolanabil404@gmail.com>
2026-06-15 17:02:40 +0300

    create MainLayout page

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar/NavBar";
+
+const MainLayout = () => {
+    return (
+        <>
+            <NavBar />
+        </>
+    );
+}
+
+export default MainLayout;
