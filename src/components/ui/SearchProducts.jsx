faea02f08e3d5c9c7455e56c3974e3af5b406c2d
Bola Nabil <bolanabil404@gmail.com>
2026-06-16 17:26:55 +0300

    add search field in navbar

---
+++ src/components/ui/SearchProducts.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SearchProducts = () => {
+    const [products, setProducts] = useState("");
+    return (
+        <div className="search-nav">
+            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            <input 
+                type="text"
+                placeholder="Search products..."
+                value={products}
+                onChange={(e) => setProducts(e.target.value)}
+            />
+        </div>
+    );
+}
+
+export default SearchProducts;
