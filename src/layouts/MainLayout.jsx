4f1e1c7326264244d5e6f53aebfe553331176322
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 19:06:49 +0300

    add Footer to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,18 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/NavBar/Footer/Footer";
 
 const MainLayout = () => {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <NavBar />
-        </>
+
+            <main className="flex-1">
+                <Outlet />
+            </main>
+
+            <Footer />
+        </div>
     );
 }
 
