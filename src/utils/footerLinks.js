206967031d2f0def106905d94021c656b4dd2e09
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 19:46:25 +0300

    add shopLinks include all links about Shop in footer

---
+++ src/utils/footerLinks.js
@@ -0,0 +1,27 @@
+export const shopLinks = [
+    {
+        "id": 1,
+        "title": "All Products",
+        "path": "/prodcuts"
+    },
+    {
+        "id": 2,
+        "title": "Categories",
+        "path": "/categories"
+    },
+    {
+        "id": 3,
+        "title": "Deals & Offers",
+        "path": "/deals"
+    },
+    {
+        "id": 4,
+        "title": "New Arrivals",
+        "path": "/new-arrivals"
+    },
+    {
+        "id": 5,
+        "title": "Best Sellers",
+        "path": "/best-sellers"
+    }
+];
