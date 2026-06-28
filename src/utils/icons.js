addefe604881ac057279ab3d325ec87806065e10
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 19:35:21 +0300

    add facebook, twiter, instagram and youtube icons

---
+++ src/utils/icons.js
@@ -0,0 +1,14 @@
+import { 
+    faFacebookF, 
+    faXTwitter,
+    faInstagram,
+    faYoutube
+} 
+from "@fortawesome/free-brands-svg-icons";
+
+export {
+    faFacebookF,
+    faXTwitter,
+    faInstagram,
+    faYoutube
+}
