2ab17bc4a220d8e0254eae2b6595ec5b1f9f2864
Bola Nabil <bolanabil404@gmail.com>
2026-06-28 17:33:36 +0300

    make code to accept all designs

--- src/components/NavBar/CenterNavLinks.jsx
+++ src/components/NavBar/CenterNavLinks.jsx
@@ -1,8 +1,17 @@
+import { centerNavLinks } from "../../utils/navLinks";
+import { NavLink } from "react-router-dom";
 
-const CenterNavLinks = () => {
+const CenterNavLinks = ({headerDesign, innderDesign}) => {
     return (
-        <>
-        </>
+        <div className={headerDesign}>
+            {
+                centerNavLinks.map((link) => (
+                    <NavLink key={link.id} to={link.path}
+                        className={({isActive}) => `${innderDesign} font-bold ${ isActive ? "active-link": ""}`}
+                    >{link.title}</NavLink>
+                ))
+            }
+        </div>
     );
 }
 
