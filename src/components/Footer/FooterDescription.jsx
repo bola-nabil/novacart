c54f39737185c0ff6479ac3a20f8f5e41f00a0f8
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 13:14:46 +0300

    add description

--- src/components/Footer/FooterDescription.jsx
+++ src/components/Footer/FooterDescription.jsx
@@ -12,6 +12,16 @@ const FooterDescription = () => {
             <div>
                 <img src={Logo} alt="logo"/>
             </div>
+
+            <div>
+                <p>
+                    Your one-stop Shop for the best products
+                    at unbeatable prices. Qualiy you can trust,
+                    every day.
+                </p>
+            </div>
+
+            
        </div>
     );
 }
