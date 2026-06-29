12a5ba1e972857cf372dfac07020bf964a91658b
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 13:58:22 +0300

    add description

--- src/components/Footer/FooterContact.jsx
+++ src/components/Footer/FooterContact.jsx
@@ -2,6 +2,14 @@ const FooterContact = () => {
     return (
         <section>
             <h1>Newsletter</h1>
+
+            <div>
+                <p>
+                    Subscribe to get special offers,
+                    free giveaways, and once-in-a-lifetime
+                    deals.
+                </p>
+            </div>
         </section>
     );
 }
