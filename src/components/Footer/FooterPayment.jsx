3cf497261cd2d222bfda4511f8af03941935140f
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 15:04:53 +0300

    add payment links

--- src/components/Footer/FooterPayment.jsx
+++ src/components/Footer/FooterPayment.jsx
@@ -1,10 +1,23 @@
 import { currentYear } from "../../utils/currentYear";
+import { paymentLinks } from "../../utils/footerLinks";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FooterPayment = () => {
     return (
         <section>
             <div>
                  <p>&copy; {currentYear()} Bola Nabil - All rights reserved.</p>
+            </div>
+
+            <div>
+                {
+                    paymentLinks.map((payment) => (
+                        <Link key={payment.id} to={payment.path}>
+                            <FontAwesomeIcon icon={payment.icon} />
+                        </Link>
+                    ))
+                }
             </div>
         </section>
     );
