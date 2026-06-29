855157c3bc50fa8acb10a678fbcdf35f589bc42e
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 15:16:50 +0300

    add pages links

--- src/components/Footer/FooterPages.jsx
+++ src/components/Footer/FooterPages.jsx
@@ -1,7 +1,19 @@
-const FooterPages = () => {
+import { Link } from "react-router-dom";
+
+const FooterPages = ({pageTitle, pages}) => {
     return (
         <section>
-            
+            <h1>{pageTitle}</h1>
+
+            <div>
+                {
+                    pages.map((page) => (
+                        <Link key={page.id} to={page.path}>
+                            {page.title}
+                        </Link>
+                    ))
+                }
+            </div>
         </section>
     );
 }
