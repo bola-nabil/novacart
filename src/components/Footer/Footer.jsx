38732016a79709478531ad4751eb902f2309412c
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 15:58:26 +0300

    Implement Container to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,25 +3,28 @@ import FooterPages from "./FooterPages";
 import { shopLinks, customerServiceLinks, companyLinks } from "../../utils/footerLinks";
 import FooterContact from "./FooterContact";
 import FooterPayment from "./FooterPayment";
+import Container from "../ui/Container";
 import "./footer.css";
 
 const Footer = () => {
     return (
         <footer>
-            <div>
-                <FooterDescription />
-
+           <Container>
                 <div>
-                    <FooterPages pageTitle="Shop" pages={shopLinks} />
-                    <FooterPages pageTitle="Customer Service" pages={customerServiceLinks} />
-                    <FooterPages pageTitle="Company" pages={companyLinks} />
-                </div>
+                    <FooterDescription />
 
-                <FooterContact />
-            </div>
-            <div>
-                <FooterPayment />
-            </div>
+                    <div>
+                        <FooterPages pageTitle="Shop" pages={shopLinks} />
+                        <FooterPages pageTitle="Customer Service" pages={customerServiceLinks} />
+                        <FooterPages pageTitle="Company" pages={companyLinks} />
+                    </div>
+
+                    <FooterContact />
+                </div>
+                <div>
+                    <FooterPayment />
+                </div>
+           </Container>
         </footer>
     );
 }
