1773b6ec8a88dce10e7bc555d42b28043a69b959
Bola Nabil <bolanabil404@gmail.com>
2026-06-29 15:24:24 +0300

    add pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 import FooterDescription from "./FooterDescription";
-
+import FooterPages from "./FooterPages";
+import { shopLinks, customerServiceLinks, companyLinks } from "../../utils/footerLinks";
 
 const Footer = () => {
     return (
         <footer>
             <div>
                 <FooterDescription />
+
+                <div>
+                    <FooterPages pageTitle="Shop" pages={shopLinks} />
+                    <FooterPages pageTitle="Customer Service" pages={customerServiceLinks} />
+                    <FooterPages pageTitle="Company" pages={companyLinks} />
+                </div>
 
             </div>
             <div>
