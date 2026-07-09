4510ce5553f3d30b2ed500e9deab27187373ba3f
Bola Nabil <bolanabil404@gmail.com>
2026-07-09 20:23:17 +0300

    edit with of email input and make block in medium screens

--- src/components/Footer/FooterContact.jsx
+++ src/components/Footer/FooterContact.jsx
@@ -16,14 +16,14 @@ const FooterContact = () => {
                 </p>
             </div>
 
-            <div className="flex">
+            <div className="lg:flex md:block">
                 <input 
                     type="email"
                     name="email"
                     placeholder="Enter your email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="outline-none border border-black p-2 h-[35px]"
+                    className="outline-none border border-black p-2 h-[35px] w-full md:w-3.5/4"
                 />
                 <div className="bg-blue-700 w-[35px] h-[35px] flex items-center justify-center rounded-md">
                     <FontAwesomeIcon icon={faPaperPlane} className="text-white" />
