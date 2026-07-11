2d18c55060e2b31b66d09a17b41e8f21597ae622
Bola Nabil <bolanabil404@gmail.com>
2026-07-11 15:09:32 +0300

    handle icons show

--- src/components/Footer/FooterPages.jsx
+++ src/components/Footer/FooterPages.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { faChevronDown } from "../../utils/icons";
+import { faChevronDown, faChevronUp} from "../../utils/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const FooterPages = ({pageTitle, pages}) => {
@@ -14,8 +14,12 @@ const FooterPages = ({pageTitle, pages}) => {
 
             <div className="flex justify-between">
                 <h1 className="text-xl md:text-lg font-bold pb-0 sm:pb-5">{pageTitle}</h1>
-                <div className="block sm:hidden">
-                    <FontAwesomeIcon icon={faChevronDown} />
+                <div className="block sm:hidden" onClick={handleShow}>
+                    {
+                        show? (<FontAwesomeIcon icon={faChevronUp} />)
+                        : 
+                        (<FontAwesomeIcon icon={faChevronDown} />)
+                    }
                 </div>
             </div>
 
