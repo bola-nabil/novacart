fbb2431b3a8a53a3472f52a4dc9abd732ddb7f52
Bola Nabil <bolanabil404@gmail.com>
2026-07-11 18:29:16 +0300

    Hero Section

---
+++ src/pages/Home/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import Container from "../../../components/ui/Container";
+
+const Hero = () => {
+    return (
+        <section className="bg-blue-100 h-screen w-sreen">
+            <Container>
+                <h1>hero</h1>
+            </Container>
+        </section>
+    );
+}
+
+export default Hero;
