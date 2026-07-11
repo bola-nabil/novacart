48c45b40bd1ed70cc9db1a996741d86219f486d5
Bola Nabil <bolanabil404@gmail.com>
2026-07-11 18:28:47 +0300

    use Hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Hero from "./sections/Hero";
+
 const Home = () => {
     return (
-        <>
-            <h1>Home Page</h1>
-        </>
+        <div>
+            <Hero />
+        </div>
     );
 }
 
