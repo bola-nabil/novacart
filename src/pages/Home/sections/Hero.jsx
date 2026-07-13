672681e72c833d96d7637cd518190790a2b172e2
Bola Nabil <bolanabil404@gmail.com>
2026-07-13 17:52:56 +0300

    import css file

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -1,10 +1,32 @@
 import Container from "../../../components/ui/Container";
+import { currentYear } from "../../../utils/currentYear";
+import Button from "../../../components/ui/buttons/Button";
+import HeroImage from "../../../assets/images/hero-image.png";
 
 const Hero = () => {
     return (
-        <section className="bg-blue-100 h-screen w-sreen">
+        <section className="bg-blue-100 h-screen w-screen">
             <Container>
-                <h1>hero</h1>
+                <div>
+                    <div>
+                        <p>Summer collection {currentYear()}</p>
+                        <h1>Find everything you need</h1>
+                        <p>
+                            Discover premium products at unbeatable prices.
+                            Quality you can trust, every day.
+                        </p>
+                        <div>
+                            <Button title="Shop Now" path="/shop" />
+                            <Button title="Explore Deals" path="/deals" color="white" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <img src={HeroImage} alt="hero product"/>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </section>
     );
