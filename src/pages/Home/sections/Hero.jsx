4870ada66a5451bfa4f0fd2c78d87f5bf99422bd
Bola Nabil <bolanabil404@gmail.com>
2026-07-13 19:57:07 +0300

    make desing responsive in all screens

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -5,9 +5,9 @@ import HeroImage from "../../../assets/images/hero-image.png";
 
 const Hero = () => {
     return (
-        <section className="bg-[#f5f7fd] h-screen w-screen">
+        <section className="hero-section bg-[#f5f7fd] w-screen">
             <Container>
-                <div className="flex items-center h-screen">
+                <div className="flex flex-col-reverse sm:flex-row items-center justify-end gap-10 sm:gap-0 py-5 sm:py-0  h-screen">
                     <div>
                         <p className="text-blue-700 font-bold text-xl">Summer collection {currentYear()}</p>
                         <h1 className="text-xl md:text-4xl lg:text-6xl font-bold py-4">Find everything you need</h1>
@@ -22,9 +22,9 @@ const Hero = () => {
                         </div>
                     </div>
 
-                    <div className="bg-[#e2ecfb] w-[600px] h-[600px] rounded-full flex items-center justify-center overflow-hidden">
-                        <div className="">
-                            <img src={HeroImage} alt="hero product" className="w-[350px] h-auto object-contai"/>
+                    <div className="bg-[#e2ecfb] w-[200px] h-[200px] sm:w-[400px] sm:h-[400px] lg:w-[600px] lg:h-[600px] rounded-full flex items-center justify-center overflow-hidden">
+                        <div>
+                            <img src={HeroImage} alt="hero product" className="w-[100px] sm:w-[250px] lg:w-[350px] h-auto object-contai"/>
                         </div>
                     </div>
                 </div>
