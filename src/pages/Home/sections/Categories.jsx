1e6c1c8295af1ef8a9d7527a102e8c6841998325
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 19:24:58 +0300

    edit api path

--- src/pages/Home/sections/Categories.jsx
+++ src/pages/Home/sections/Categories.jsx
@@ -1,4 +1,12 @@
+import { useCategories } from "../../../hooks/useCategories";
+
 const Categories = () => {
+    const { data, isLoading, error } = useCategories();
+
+    if(isLoading) return <p>Loading...</p>
+    if (error) return <p>Something went wrong.</p>;
+
+    console.log(data);
     return (
         <section>
             <h1>Categories</h1>
