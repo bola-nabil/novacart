d19d27b86299d5b5f7b1b35ee3ffd614facfcc5c
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 17:45:10 +0300

    add categories section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
+import Categories from "./sections/Categories";
 import "./home.css";
 
 const Home = () => {
     return (
         <div>
             <Hero />
-
             <Features />
+            <Categories />
         </div>
     );
 }
