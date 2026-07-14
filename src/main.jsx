aaf00aceb918f7cb82568b7888e4ed25b7b0487d
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 18:07:24 +0300

    wrap tanstack in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient.js";
 import "./assets/styles/globals.css";
 import router from './routes/router.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={router}/>
+    </StrictMode>,
+  </QueryClientProvider>
 )
