f4519ea2f305afa66144c48cb31030413c46968e
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 16:44:28 +0300

    add features section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Hero from "./sections/Hero";
+import Features from "./sections/Features";
 import "./home.css";
 
 const Home = () => {
     return (
         <div>
             <Hero />
+
+            <Features />
         </div>
     );
 }
