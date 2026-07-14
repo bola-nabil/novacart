b6b2d57ce2c817b1794c9fc218b50fa03c19d072
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 14:43:09 +0300

    Edit responsive design in mobile screens

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -5,9 +5,9 @@ import HeroImage from "../../../assets/images/hero-image.png";
 
 const Hero = () => {
     return (
-        <section className="hero-section bg-[#f5f7fd] w-screen">
+        <section className="hero-section bg-[#f5f7fd] w-full">
             <Container>
-                <div className="flex flex-col-reverse sm:flex-row items-center justify-end gap-10 sm:gap-0 py-5 sm:py-0  h-screen">
+                <div className="flex flex-col-reverse sm:flex-row items-center justify-center sm:justify-center gap-10 sm:gap-0 hero-section">
                     <div>
                         <p className="text-blue-700 font-bold text-xl">Summer collection {currentYear()}</p>
                         <h1 className="text-xl md:text-4xl lg:text-6xl font-bold py-4">Find everything you need</h1>
