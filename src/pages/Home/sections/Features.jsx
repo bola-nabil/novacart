d07f7b3a1a709ec7f8e09ad203b3dc9817ef6e4c
Bola Nabil <bolanabil404@gmail.com>
2026-07-14 17:06:53 +0300

    add featuresContent include all data in features section

--- src/pages/Home/sections/Features.jsx
+++ src/pages/Home/sections/Features.jsx
@@ -1,3 +1,32 @@
+import { faTruck, faShield, faArrowsRotate, faHeadset } from "../../../utils/icons";
+
+const featurseContent = [
+    {
+        id: 1,
+        icon: faTruck,
+        header: "Free Shipping",
+        description: "On orders over $50"
+    },
+    {
+        id: 2,
+        icon: faShield,
+        header: "Secure Payment",
+        description: "100% secure payment"
+    },
+    {
+        id: 3,
+        icon: faArrowsRotate,
+        header: "Easy Returns",
+        description: "30 days returns policy"
+    },
+    {
+        id: 4,
+        icon: faHeadset,
+        header: "24/7 Support",
+        description: "Dedlicated support"
+    },
+];
+
 const Features = () => {
     return(
         <section>
