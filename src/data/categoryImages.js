aa372df8c6f5c60786bca9a1e69aace4617c7417
Bola Nabil <bolanabil404@gmail.com>
2026-07-15 12:46:18 +0300

    Export categories images

---
+++ src/data/categoryImages.js
@@ -0,0 +1,15 @@
+import beauty from "../assets/images/categories/beauty.webp";
+import fragrances from "../assets/images/categories/fragrances.webp";
+import furniture from "../assets/images/categories/furniture.webp";
+import groceries from "../assets/images/categories/groceries.webp";
+import HomeDecoration from "../assets/images/categories/home-decoration.webp";
+
+
+
+export const categoryImages = {
+    beauty,
+    fragrances,
+    furniture,
+    groceries,
+    HomeDecoration
+}
