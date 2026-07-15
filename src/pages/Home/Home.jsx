c4fc48c1dca2e564623d07790cf446a2f2d0febd
Bola Nabil <bolanabil404@gmail.com>
2026-07-15 15:23:47 +0300

    add product feature section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Categories from "./sections/Categories";
+import FeatureProducts from "./sections/FeatureProducts";
 import "./home.css";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Hero />
             <Features />
             <Categories />
+            <FeatureProducts />
         </div>
     );
 }
