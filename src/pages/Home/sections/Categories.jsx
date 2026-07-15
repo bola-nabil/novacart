a80e730c9a43b3086a52877af316a0a3d406c3ff
Bola Nabil <bolanabil404@gmail.com>
2026-07-15 12:59:35 +0300

    loop for categories data

--- src/pages/Home/sections/Categories.jsx
+++ src/pages/Home/sections/Categories.jsx
@@ -1,5 +1,7 @@
 import { useCategories } from "../../../hooks/useCategories";
 import Container from "../../../components/ui/Container";
+import { categoryImages } from "../../../data/categoryImages";
+import { Link } from "react-router-dom";
 
 const Categories = () => {
     const { data, isLoading, error } = useCategories();
@@ -12,7 +14,22 @@ const Categories = () => {
             <Container>
                 <div className="flex justify-between">
                     <h1 className="text-md sm:text-xl font-bold">Shop by Category</h1>
-                    <p className="text-blue-700">View All</p>
+                    <Link to="/categories" className="text-blue-700">View All</Link>
+                </div>
+
+                <div>
+                    {
+                        data.map((category) => (
+                            <div key={category.slug}>
+                                <h1>{category.name}</h1>
+                                <div>
+                                    <Link to={`/products/category/${category.slug}`}>
+                                        <img src={categoryImages[category.slug]} alt={category.name}/>
+                                    </Link>
+                                </div>
+                            </div>
+                        )).slice(0,5)
+                    }
                 </div>
             </Container>
         </section>
