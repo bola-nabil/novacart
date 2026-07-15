0b9654c180bf12f669457b44866c9e994ed95ca1
Bola Nabil <bolanabil404@gmail.com>
2026-07-15 19:28:02 +0300

    add ProductDeatails to show product info

---
+++ src/pages/Product/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+    const {id} = useParams();
+    return (
+        <>
+        <h1>Product ID: {id}</h1>
+        </>
+    );
+}
+
+export default ProductDetails;
