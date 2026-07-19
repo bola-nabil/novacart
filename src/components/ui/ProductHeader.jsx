9b8adae05d6226c13b403658c339a9f3987f741e
Bola Nabil <bolanabil404@gmail.com>
2026-07-19 18:47:32 +0300

    Finish Implement

--- src/components/ui/ProductHeader.jsx
+++ src/components/ui/ProductHeader.jsx
@@ -1,7 +1,10 @@
-const ProductHeader = () => {
+import { Link } from "react-router-dom";
+
+const ProductHeader = ({title, path, linkColor="black"}) => {
     return (
-        <div>
-            
+        <div className="flex justify-between items-center font-bold">
+            <h1 className="text-2xl">{title}</h1>
+            <Link to={path} className={`text-md text-${linkColor}`}>View All</Link>
         </div>
     );
 }
