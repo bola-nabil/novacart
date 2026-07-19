47dd542fd87d7d2a07c5bee043911d23cb03d03f
Bola Nabil <bolanabil404@gmail.com>
2026-07-19 18:12:49 +0300

    Use API data to get product details

--- src/pages/Home/sections/BestSellers.jsx
+++ src/pages/Home/sections/BestSellers.jsx
@@ -1,7 +1,22 @@
+import Container from "../../../components/ui/Container";
+import { useProducts } from "../../../hooks/useProducts";
+
 const BestSellers = () => {
+    const { data, isLoading, error} = useProducts();
+
+    const bestSellers = data?.products
+        ?.sort((a, b) => b.rating - a.rating)
+        .slice(0, 8);
+    
+    if(isLoading) return <p>Loading...</p>
+    if(error) return <p>Something went wrong</p>
+
+    console.log(bestSellers);
     return (
         <section>
-            <h1>Best Sellers</h1>
+            <Container>
+
+            </Container>
         </section>
     );
 }
