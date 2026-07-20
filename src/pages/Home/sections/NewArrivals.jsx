732825d4e6d1dc7ee64e2db903813d7012ac87c4
Bola Nabil <bolanabil404@gmail.com>
2026-07-20 18:28:04 +0300

    Use API data to get product details

--- src/pages/Home/sections/NewArrivals.jsx
+++ src/pages/Home/sections/NewArrivals.jsx
@@ -1,6 +1,14 @@
 import Container from "../../../components/ui/Container";
+import { useProducts } from "../../../hooks/useProducts";
 
 const NewArrivals = () => {
+    const { data, isLoading, error} = useProducts();
+    const newArrivals = data?.products.slice(-6);
+
+    if(isLoading) return <p>Loading...</p>
+    if(error) return <p>Something went wrong</p>
+
+    console.log("new arrivals data:", data);
     return (
         <section>
             <Container>
