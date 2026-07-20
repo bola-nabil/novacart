2f5d028ed46b6086fa98cd2a6f1be56320193130
Bola Nabil <bolanabil404@gmail.com>
2026-07-20 19:17:04 +0300

    Implement function component

---
+++ src/pages/Home/sections/Brands.jsx
@@ -0,0 +1,13 @@
+import Container from "../../../components/ui/Container";
+
+const Brands = () => {
+    return (
+        <section className="py-10">
+            <Container>
+                <h1>Top Brands</h1>
+            </Container>
+        </section>
+    );
+}
+
+export default Brands;
