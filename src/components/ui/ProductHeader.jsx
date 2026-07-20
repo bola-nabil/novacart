e46d1a5f25cada98288ec767b7f706887e8d34a2
Bola Nabil <bolanabil404@gmail.com>
2026-07-20 18:45:46 +0300

    add degree of color in component

--- src/components/ui/ProductHeader.jsx
+++ src/components/ui/ProductHeader.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-const ProductHeader = ({title, path, linkColor="black"}) => {
+const ProductHeader = ({title, path, linkColor}) => {
     return (
         <div className="flex justify-between items-center font-bold">
             <h1 className="text-2xl">{title}</h1>
-            <Link to={path} className={`text-md text-${linkColor}`}>View All</Link>
+            <Link to={path} className={`text-md text-${linkColor}-700`}>View All</Link>
         </div>
     );
 }
