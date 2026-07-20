320cd8d167d4215f9d8088844cbbb51fb269e747
Bola Nabil <bolanabil404@gmail.com>
2026-07-20 18:22:28 +0300

    Implement function component

---
+++ src/pages/Home/sections/NewArrivals.jsx
@@ -0,0 +1,13 @@
+import Container from "../../../components/ui/Container";
+
+const NewArrivals = () => {
+    return (
+        <section>
+            <Container>
+                <h1>New Arrivals</h1>
+            </Container>
+        </section>
+    );
+}
+
+export default NewArrivals;
