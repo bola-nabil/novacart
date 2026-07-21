8a12507a36112b07e7f5a1dd49cf5edb6e8d36b7
Bola Nabil <bolanabil404@gmail.com>
2026-07-21 19:14:23 +0300

    Add array include object of brand images

---
+++ src/data/brandsImages.js
@@ -0,0 +1,27 @@
+import Apple from "../assets/images/brands/apple.webp";
+import Samsung from "../assets/images/brands/samsung.webp";
+import Sony from "../assets/images/brands/sony.webp";
+import Adidas from "../assets/images/brands/adidas.webp";
+
+export const brandsImages = [
+    {
+        id: 1,
+        name: "Apple",
+        image: Apple
+    },
+    {
+        id: 2,
+        name: "Samsung",
+        image: Samsung
+    },
+    {
+        id: 3,
+        name: "Sony",
+        image: Sony
+    },
+    {
+        id: 4,
+        name: "Adidas",
+        image: Adidas
+    }
+];
