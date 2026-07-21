2386a40d5655fae65345dcda6afae79fd02a04b6
Bola Nabil <bolanabil404@gmail.com>
2026-07-21 20:10:56 +0300

    Implement function component

---
+++ src/pages/Home/sections/Testimonials.jsx
@@ -0,0 +1,13 @@
+import Container from "../../../components/ui/Container";
+
+const Testimonials = () => {
+    return (
+        <section className="py-10">
+            <Container>
+                <h1>Testimonials</h1>
+            </Container>
+        </section>
+    );
+}
+
+export default Testimonials;
