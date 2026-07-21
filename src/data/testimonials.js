761c4eaed2c21703a2e6b08b221eadb56fd74d37
Bola Nabil <bolanabil404@gmail.com>
2026-07-21 20:15:43 +0300

    testimonials local data

---
+++ src/data/testimonials.js
@@ -0,0 +1,29 @@
+export const testimonials = [
+  {
+    id: 1,
+    name: "Sarah Johnson",
+    role: "Verified Customer",
+    image: "https://i.pravatar.cc/150?img=1",
+    rating: 5,
+    comment:
+      "Amazing products and fast delivery. I had a great shopping experience!"
+  },
+  {
+    id: 2,
+    name: "Michael Brown",
+    role: "Verified Customer",
+    image: "https://i.pravatar.cc/150?img=2",
+    rating: 5,
+    comment:
+      "The quality was excellent and the prices were very reasonable."
+  },
+  {
+    id: 3,
+    name: "Emily Davis",
+    role: "Verified Customer",
+    image: "https://i.pravatar.cc/150?img=3",
+    rating: 4,
+    comment:
+      "The website is easy to use and my order arrived quickly."
+  }
+];
