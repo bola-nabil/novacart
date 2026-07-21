a3c5658203f59c10a37c0199ae4af3c3d2a9ac34
Bola Nabil <bolanabil404@gmail.com>
2026-07-21 19:43:14 +0300

    loop for images and make styles

--- src/pages/Home/sections/Brands.jsx
+++ src/pages/Home/sections/Brands.jsx
@@ -1,10 +1,20 @@
 import Container from "../../../components/ui/Container";
+import { brandsImages } from "../../../data/brandsImages";
 
 const Brands = () => {
     return (
         <section className="py-10">
             <Container>
-                <h1>Top Brands</h1>
+                <h1 className="text-2xl font-bold">Top Brands</h1>
+                <div className="pt-7 flex items-center justify-between">
+                    {
+                        brandsImages.map((brand) => (
+                            <div key={brand.id} className="bg-gray-100 rounded-lg flex items-center justify-center gap-4 p-3 lg:p-5">
+                                <img src={brand.image} alt={brand.name} className="w-[50px] h-[50px] sm:w-[150px] sm:h-[100px] object-contain"/>
+                            </div>
+                        ))
+                    }
+                </div>
             </Container>
         </section>
     );
